d29c1a1c69ddb8885047b8aeedfe121ee9671219
iMAC <treyrosius@gmail.com>
2021-05-23 10:20:45 +0100

    service

--- components/Login.vue
+++ components/Login.vue
@@ -9,7 +9,7 @@
         <div class="right">
          <amplify-authenticator v-if="authState !== 'signedin'" class="signIn" />
           <div v-if="authState === 'signedin' && user">
-         <div>Added login and signup</div>
+         <div>Added login signup</div>
        <sample/>
      <button v-on:click="signOut">Sign Out</button>
     </div>
