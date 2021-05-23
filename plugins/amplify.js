5ebce8fb24087ab40d64f16651233691ca93c939
iMAC <treyrosius@gmail.com>
2021-05-23 09:19:02 +0100

    added src

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,5 +1,5 @@
 // plugins/amplify.js
 import Amplify,{ Storage } from 'aws-amplify'
 import '@aws-amplify/ui-vue';
-import config from '../src/aws-exports'
+import config from '../src/aws-exports';
 Amplify.configure(config)
